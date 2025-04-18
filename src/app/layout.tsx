eeb7b4cf2053a64b0724026d5da81bb3ba8091e0
Zoe <carol331536@gmail.com>
2025-04-18 06:06:01 +0200

    feat: add background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import BootstrapClient from "@/components/BootstrapClient";
-import "./custom.scss";
+import "../styles/global.scss";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
